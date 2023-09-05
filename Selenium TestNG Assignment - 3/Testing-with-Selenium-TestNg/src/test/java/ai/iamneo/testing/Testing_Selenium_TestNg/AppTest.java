9fc6d5faa083fca7aca68147301e6f19e2239cb3
Iamneo <techadmin@iamneo.ai>
2023-09-05 13:00:42 +0000

    Backup Commit

--- Selenium TestNG Assignment - 3/Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
+++ Selenium TestNG Assignment - 3/Testing-with-Selenium-TestNg/src/test/java/ai/iamneo/testing/Testing_Selenium_TestNg/AppTest.java
@@ -3,8 +3,11 @@ package ai.iamneo.testing.Testing_Selenium_TestNg;
 import org.testng.annotations.Test;
 import java.net.URL;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.Assert;
@@ -33,6 +36,11 @@ public class AppTest {
 	public void testcase_2() throws InterruptedException 
       {
 	       //write Your Code here to Login
+		   WebElement frm=driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+		   driver.switchTo().frame(frm);
+		   WebElement source=driver.findElement(By.id("draggable"));
+		   WebElement destination=driver.findElement(By.id("droppable"));
+		   Actions act=new Actions
                
 		 String get = "Dropped or not"; //Get the text of Dropped
 		 Assert.assertEquals(get, " ");
